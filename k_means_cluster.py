a86b37e9cc25c8364348e7f4ed5c5406e00079af
Valerie Gorbik <valerianka@me.com>
2016-07-15 22:54:37 -0700

    Complete Enron dataset clustering project
    Completed clustering mini-project on financial dataset with feature scaling.

---
+++ k_means_cluster.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python 
+
+""" 
+    Skeleton code for k-means clustering mini-project.
+"""
+
+
+
+
+import pickle
+import numpy
+import matplotlib.pyplot as plt
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+from sklearn.cluster import KMeans
+
+
+
+def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
+    """ some plotting code designed to help you visualize your clusters """
+
+    ### plot each cluster with a different color--add more colors for
+    ### drawing more than five clusters
+    colors = ["b", "c", "k", "m", "g"]
+    for ii, pp in enumerate(pred):
+        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
+
+    ### if you like, place red stars over points that are POIs (just for funsies)
+    if mark_poi:
+        for ii, pp in enumerate(pred):
+            if poi[ii]:
+                plt.scatter(features[ii][0], features[ii][1], color="r", marker="*")
+    plt.xlabel(f1_name)
+    plt.ylabel(f2_name)
+    plt.savefig(name)
+    plt.show()
+
+
+
+### load in the dict of dicts containing all the data on each person in the dataset
+data_dict = pickle.load( open("final_project_dataset.pkl", "r") )
+### there's an outlier--remove it! 
+data_dict.pop("TOTAL", 0)
+# keyss = data_dict.keys()
+#print data_dict[keyss[0]].keys()
+# for k in keyss:
+#     print data_dict[k]['exercised_stock_options']
+# print data_dict[keyss[1]]
+salary_values = [data_dict[k]['salary'] for k in data_dict.keys()]
+max_salary = 0
+for value in salary_values:
+    if (type(value) == int and (value > max_salary)):
+        max_salary = value
+
+for name in data_dict.keys():
+    if (type(data_dict[name]['salary']) == int):
+        data_dict[name]['salary'] = data_dict[name]['salary'] / float(max_salary)
+
+ex_stock_values = [data_dict[k]['exercised_stock_options'] for k in data_dict.keys()]
+max_ex_stock_value = 0
+for value in ex_stock_values:
+    if (type(value) == int and (value > max_ex_stock_value)):
+        max_ex_stock_value = value
+
+for name in data_dict.keys():
+    if (type(data_dict[name]['exercised_stock_options']) == int):
+        data_dict[name]['exercised_stock_options'] = data_dict[name]['exercised_stock_options'] / float(max_ex_stock_value)
+
+### the input features we want to use 
+### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+feature_1 = "salary"
+feature_2 = "exercised_stock_options"
+#feature_3 = "total_payments"
+poi  = "poi"
+features_list = [poi, feature_1, feature_2]
+data = featureFormat(data_dict, features_list )
+poi, finance_features = targetFeatureSplit( data )
+
+### in the "clustering with 3 features" part of the mini-project,
+### you'll want to change this line to 
+### for f1, f2, _ in finance_features:
+### (as it's currently written, the line below assumes 2 features)
+for f1, f2 in finance_features:
+    plt.scatter( f1, f2 )
+plt.show()
+
+### cluster here; create predictions of the cluster labels
+### for the data and store them to a list called pred
+cls = KMeans(2)
+pred = cls.fit_predict(data)
+
+
+
+### rename the "name" parameter when you change the number of features
+### so that the figure gets saved to a different file
+try:
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+except NameError:
+    print "no predictions object named pred found, no clusters to plot"
